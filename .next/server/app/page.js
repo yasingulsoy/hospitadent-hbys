fba90fa2e42f48151dc525edc31614db859c0b8b
Yasin Gülsoy <yasingulsoy02@gmail.com>
2025-08-12 11:53:03 +0300

    Proje dosyaları güncellendi; önceden oluşturulmuş manifest dosyaları ve önbellek dosyaları değiştirildi, bazı sayfa bileşenleri silindi ve yeni sayfa referans manifestleri eklendi.

--- .next/server/app/page.js
+++ .next/server/app/page.js
@@ -266,7 +266,7 @@ module.exports = require("util");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, ["vendor-chunks/next","vendor-chunks/lucide-react","vendor-chunks/@swc"], () => (__webpack_exec__("(rsc)/./node_modules/next/dist/build/webpack/loaders/next-app-loader/index.js?name=app%2Fpage&page=%2Fpage&appPaths=%2Fpage&pagePath=private-next-app-dir%2Fpage.tsx&appDir=C%3A%5CUsers%5Cyasin%5COneDrive%5CMasa%C3%BCst%C3%BC%5Chbys%5Chospitadent-hbys%5Csrc%5Capp&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js&rootDir=C%3A%5CUsers%5Cyasin%5COneDrive%5CMasa%C3%BCst%C3%BC%5Chbys%5Chospitadent-hbys&isDev=true&tsconfigPath=tsconfig.json&basePath=&assetPrefix=&nextConfigOutput=&preferredRegion=&middlewareConfig=e30%3D&isGlobalNotFoundEnabled=!")));
+var __webpack_exports__ = __webpack_require__.X(0, ["vendor-chunks/next","vendor-chunks/@swc","vendor-chunks/lucide-react"], () => (__webpack_exec__("(rsc)/./node_modules/next/dist/build/webpack/loaders/next-app-loader/index.js?name=app%2Fpage&page=%2Fpage&appPaths=%2Fpage&pagePath=private-next-app-dir%2Fpage.tsx&appDir=C%3A%5CUsers%5Cyasin%5COneDrive%5CMasa%C3%BCst%C3%BC%5Chbys%5Chospitadent-hbys%5Csrc%5Capp&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js&rootDir=C%3A%5CUsers%5Cyasin%5COneDrive%5CMasa%C3%BCst%C3%BC%5Chbys%5Chospitadent-hbys&isDev=true&tsconfigPath=tsconfig.json&basePath=&assetPrefix=&nextConfigOutput=&preferredRegion=&middlewareConfig=e30%3D&isGlobalNotFoundEnabled=!")));
 module.exports = __webpack_exports__;
 
 })();
